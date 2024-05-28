86ef6ca2e086709704fcf55e7e37c2b131fb37eb
sepehr ariamehr <sepehrariamehr@hotmail.com>
2024-05-28 23:34:58 +0330

    Add project files.

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿using System.Windows.Forms;
+
+namespace Puzzle
+{
+	public partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private Timer HelpTimer;
+
+		private Timer ProgressTimer;
+		private Panel puzzlePanel;
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			puzzlePanel = new System.Windows.Forms.Panel();
+			HelpTimer = new System.Windows.Forms.Timer(components);
+			ProgressTimer = new System.Windows.Forms.Timer(components);
+			toolStrip1 = new System.Windows.Forms.ToolStrip();
+			TsProgress = new System.Windows.Forms.ToolStripProgressBar();
+			toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			btnGameSetting = new System.Windows.Forms.ToolStripSplitButton();
+			toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			numRow = new System.Windows.Forms.ToolStripComboBox();
+			toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			numCol = new System.Windows.Forms.ToolStripComboBox();
+			toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			btnApply = new System.Windows.Forms.ToolStripMenuItem();
+			btnCancel = new System.Windows.Forms.ToolStripButton();
+			btnShowHelp = new System.Windows.Forms.ToolStripButton();
+			splitContainer1 = new System.Windows.Forms.SplitContainer();
+			toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+			splitContainer1.Panel1.SuspendLayout();
+			splitContainer1.Panel2.SuspendLayout();
+			splitContainer1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// puzzlePanel
+			// 
+			puzzlePanel.AutoSize = true;
+			puzzlePanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			puzzlePanel.BackColor = System.Drawing.SystemColors.Control;
+			puzzlePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			puzzlePanel.Location = new System.Drawing.Point(0, 0);
+			puzzlePanel.Name = "puzzlePanel";
+			puzzlePanel.Size = new System.Drawing.Size(404, 75);
+			puzzlePanel.TabIndex = 1;
+			// 
+			// HelpTimer
+			// 
+			HelpTimer.Interval = 2000;
+			HelpTimer.Tick += timer1_Tick;
+			// 
+			// ProgressTimer
+			// 
+			ProgressTimer.Interval = 50;
+			ProgressTimer.Tick += timer2_Tick;
+			// 
+			// toolStrip1
+			// 
+			toolStrip1.BackColor = System.Drawing.Color.Gray;
+			toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+			toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { TsProgress, toolStripSeparator1, btnGameSetting, btnCancel, btnShowHelp });
+			toolStrip1.Location = new System.Drawing.Point(0, 0);
+			toolStrip1.Name = "toolStrip1";
+			toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+			toolStrip1.Size = new System.Drawing.Size(404, 32);
+			toolStrip1.Stretch = true;
+			toolStrip1.TabIndex = 4;
+			toolStrip1.Text = "toolStrip1";
+			// 
+			// TsProgress
+			// 
+			TsProgress.Name = "TsProgress";
+			TsProgress.Padding = new System.Windows.Forms.Padding(10, 0, 10, 10);
+			TsProgress.Size = new System.Drawing.Size(120, 29);
+			// 
+			// toolStripSeparator1
+			// 
+			toolStripSeparator1.Name = "toolStripSeparator1";
+			toolStripSeparator1.Size = new System.Drawing.Size(6, 32);
+			// 
+			// btnGameSetting
+			// 
+			btnGameSetting.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripMenuItem1, numRow, toolStripSeparator2, toolStripMenuItem2, numCol, toolStripSeparator3, btnApply });
+			btnGameSetting.Image = Properties.Resources.start;
+			btnGameSetting.ImageTransparentColor = System.Drawing.Color.Magenta;
+			btnGameSetting.Name = "btnGameSetting";
+			btnGameSetting.Size = new System.Drawing.Size(105, 29);
+			btnGameSetting.Text = "تنظیمات پازل";
+			// 
+			// toolStripMenuItem1
+			// 
+			toolStripMenuItem1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			toolStripMenuItem1.Enabled = false;
+			toolStripMenuItem1.Image = Properties.Resources.RowPic;
+			toolStripMenuItem1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			toolStripMenuItem1.Name = "toolStripMenuItem1";
+			toolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
+			toolStripMenuItem1.Text = "تعداد سطر ها";
+			// 
+			// numRow
+			// 
+			numRow.AutoCompleteCustomSource.AddRange(new string[] { "1", "2", "3", "4", "5", "6", "7" });
+			numRow.BackColor = System.Drawing.SystemColors.ButtonFace;
+			numRow.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			numRow.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+			numRow.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7" });
+			numRow.Name = "numRow";
+			numRow.Size = new System.Drawing.Size(121, 23);
+			numRow.Sorted = true;
+			// 
+			// toolStripSeparator2
+			// 
+			toolStripSeparator2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			toolStripSeparator2.Name = "toolStripSeparator2";
+			toolStripSeparator2.Size = new System.Drawing.Size(178, 6);
+			// 
+			// toolStripMenuItem2
+			// 
+			toolStripMenuItem2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			toolStripMenuItem2.Enabled = false;
+			toolStripMenuItem2.Image = Properties.Resources.columnPic;
+			toolStripMenuItem2.Name = "toolStripMenuItem2";
+			toolStripMenuItem2.Size = new System.Drawing.Size(181, 22);
+			toolStripMenuItem2.Text = "تعداد ستون ها";
+			// 
+			// numCol
+			// 
+			numCol.AutoCompleteCustomSource.AddRange(new string[] { "1", "2", "3", "4", "5", "6", "7", "8" });
+			numCol.BackColor = System.Drawing.SystemColors.ButtonFace;
+			numCol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			numCol.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+			numCol.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8" });
+			numCol.Name = "numCol";
+			numCol.Size = new System.Drawing.Size(121, 23);
+			// 
+			// toolStripSeparator3
+			// 
+			toolStripSeparator3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			toolStripSeparator3.Name = "toolStripSeparator3";
+			toolStripSeparator3.Size = new System.Drawing.Size(178, 6);
+			// 
+			// btnApply
+			// 
+			btnApply.BackColor = System.Drawing.SystemColors.ControlLight;
+			btnApply.Name = "btnApply";
+			btnApply.Size = new System.Drawing.Size(181, 22);
+			btnApply.Text = "شروع بازی";
+			btnApply.Click += btnApply_Click;
+			// 
+			// btnCancel
+			// 
+			btnCancel.Enabled = false;
+			btnCancel.Image = Properties.Resources.cancel;
+			btnCancel.ImageTransparentColor = System.Drawing.Color.Magenta;
+			btnCancel.Name = "btnCancel";
+			btnCancel.Size = new System.Drawing.Size(81, 29);
+			btnCancel.Text = "ریست پازل";
+			btnCancel.Click += btnCancel_Click;
+			// 
+			// btnShowHelp
+			// 
+			btnShowHelp.DoubleClickEnabled = true;
+			btnShowHelp.Enabled = false;
+			btnShowHelp.Image = (System.Drawing.Image)resources.GetObject("btnShowHelp.Image");
+			btnShowHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
+			btnShowHelp.Name = "btnShowHelp";
+			btnShowHelp.Size = new System.Drawing.Size(70, 29);
+			btnShowHelp.Text = "راهنمایی";
+			btnShowHelp.Click += btnShowHelp_Click;
+			// 
+			// splitContainer1
+			// 
+			splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+			splitContainer1.IsSplitterFixed = true;
+			splitContainer1.Location = new System.Drawing.Point(0, 0);
+			splitContainer1.Name = "splitContainer1";
+			splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			splitContainer1.Panel1.Controls.Add(puzzlePanel);
+			// 
+			// splitContainer1.Panel2
+			// 
+			splitContainer1.Panel2.Controls.Add(toolStrip1);
+			splitContainer1.Size = new System.Drawing.Size(404, 111);
+			splitContainer1.SplitterDistance = 75;
+			splitContainer1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			AutoScroll = true;
+			AutoSize = true;
+			AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			ClientSize = new System.Drawing.Size(404, 111);
+			Controls.Add(splitContainer1);
+			DoubleBuffered = true;
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			HelpButton = true;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			MinimumSize = new System.Drawing.Size(420, 150);
+			Name = "Form1";
+			SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			Text = "پازل من";
+			TopMost = true;
+			FormClosing += Form1_FormClosing;
+			Load += Form1_Load;
+			toolStrip1.ResumeLayout(false);
+			toolStrip1.PerformLayout();
+			splitContainer1.Panel1.ResumeLayout(false);
+			splitContainer1.Panel1.PerformLayout();
+			splitContainer1.Panel2.ResumeLayout(false);
+			splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+			splitContainer1.ResumeLayout(false);
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripProgressBar TsProgress;
+		private System.Windows.Forms.ToolStripSplitButton btnGameSetting;
+		private System.Windows.Forms.ToolStripComboBox numRow;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripComboBox numCol;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripButton btnCancel;
+		private System.Windows.Forms.ToolStripButton btnShowHelp;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ToolStripMenuItem btnApply;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+	}
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+//using ImagePuzzle.Properties;
+
+namespace Puzzle
+{
+	public partial class Form1 : Form
+	{
+		private List<PictureBox> puzzle;
+		private List<Label> cover;
+		private Label[] pushCover = new Label[2];
+		private int puzzleSize = 0, clickNumber = 0, totalImage = 30, itemSize = 90;
+		private bool allowClick = true;
+		private int rows = 0, cols = 0;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void UniqueRandomNumber(List<int> numbers, int startNumber, int endNumber, int countNumbers)
+		{
+			Random random = new Random();
+			for (int i = 0; i < countNumbers; i++)
+			{
+				int item = random.Next(startNumber, endNumber + 1);
+				while (numbers.IndexOf(item) >= 0)
+				{
+					item = random.Next(startNumber, endNumber + 1);
+				}
+				numbers.Add(item);
+			}
+		}
+
+		private void ShuffleNumber(List<int> numbers)
+		{
+			List<int> list = new List<int>();
+			List<int> list2 = new List<int>();
+			UniqueRandomNumber(list, 0, numbers.Count - 1, numbers.Count);
+			list2.AddRange(numbers);
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				numbers[i] = list2[list[i]];
+			}
+			list.Clear();
+			list2.Clear();
+		}
+
+		private void btnApply_Click(object sender, EventArgs e)
+		{
+			if (!(numRow.SelectedIndex == -1) && !(numCol.SelectedIndex == -1))
+			{
+				rows = Convert.ToInt32(numRow.SelectedItem);
+				cols = Convert.ToInt32(numCol.SelectedItem);
+
+				puzzleSize = rows * cols;
+				if (puzzleSize % 2 != 0)
+				{
+					MessageBox.Show("حاصلضرب تعداد سطرها و ستونها باید عددی زوج باشد", "ورودی نامعتبر", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+					puzzleSize = 0;
+					return;
+				}
+
+				btnGameSetting.Enabled = false;
+				btnCancel.Enabled = true;
+				numRow.Enabled = false;
+				numCol.Enabled = false;
+				btnShowHelp.Enabled = true;
+
+
+
+
+				DrawPuzzle();
+				SetFormCenter();
+				DrawProgressBar();
+				FillPuzzle();
+				DarwCover();
+
+
+			}
+			else
+			{
+				MessageBox.Show("حتما باید تعداد خانه های جدول را وارد کنید");
+			}
+
+		}
+		private void SetFormCenter()
+		{
+			Rectangle clientRect = this.ClientRectangle;
+			int width = clientRect.Width;
+			int height = clientRect.Height;
+
+			// Calculate the center of the screen
+			int screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+			int screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+			int centerX = (screenWidth - width) / 2;
+			int centerY = (screenHeight - height) / 2;
+
+			// Set the form location to the center of the screen
+			this.Location = new Point(centerX, centerY);
+		}
+		private void DrawProgressBar()
+		{
+			TsProgress.Maximum = puzzleSize / 2;
+			TsProgress.Minimum = 0;
+			TsProgress.Step = 1;
+		}
+
+		private void DarwCover()
+		{
+			cover = new List<Label>();
+			for (int i = 0; (decimal)i < rows; i++)
+			{
+				for (int j = 0; (decimal)j < cols; j++)
+				{
+					Label label = new Label();
+					label.Size = new Size(itemSize, itemSize);
+					label.Location = new Point(j * itemSize, i * itemSize);
+					label.Name = puzzle[i * cols + j].Name;
+					label.BorderStyle = BorderStyle.Fixed3D;
+					Label label2 = label;
+					cover.Add(label2);
+					puzzlePanel.Controls.Add(label2);
+					label2.BringToFront();
+					label2.Click += CoverItem_Click;
+				}
+			}
+		}
+
+		private void CoverItem_Click(object sender, EventArgs e)
+		{
+			Label label = new Label();
+			label = (Label)sender;
+			if (allowClick)
+			{
+				label.Visible = false;
+				clickNumber++;
+				if (clickNumber == 1)
+				{
+					pushCover[0] = label;
+				}
+				else
+				{
+					pushCover[1] = label;
+					allowClick = false;
+					ProgressTimer.Start();
+				}
+				btnShowHelp.Enabled = false;
+			}
+		}
+
+		private void FillPuzzle()
+		{
+			List<int> list = new List<int>();
+			List<int> list2 = new List<int>();
+			UniqueRandomNumber(list2, 1, totalImage, puzzleSize / 2);
+			for (int i = 0; i < puzzleSize / 2; i++)
+			{
+				list.Add(list2[i]);
+				list.Add(list2[i]);
+			}
+			ShuffleNumber(list);
+			for (int i = 0; i < puzzleSize; i++)
+			{
+				string path = Path.Combine(Directory.GetCurrentDirectory(), $"Images\\p_{list[i]:D2}.jpg").ToString();
+				puzzle[i].Image = Image.FromFile(path);
+				puzzle[i].Name = list[i].ToString();
+			}
+		}
+
+		private void DrawPuzzle()
+		{
+			int totalWidth = cols * itemSize + 15;
+			int totalHeight = rows * (itemSize + 13) + 40;
+
+			MinimumSize = new Size(totalWidth, totalHeight);
+			Size = new Size(totalWidth, totalHeight);
+
+
+			puzzle = new List<PictureBox>();
+			for (int i = 0; (decimal)i < rows; i++)
+			{
+				for (int j = 0; (decimal)j < cols; j++)
+				{
+					PictureBox pictureBox = new PictureBox();
+					pictureBox.Size = new Size(itemSize, itemSize);
+					pictureBox.BorderStyle = BorderStyle.None;
+					pictureBox.Location = new Point(j * itemSize, i * itemSize);
+					pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+					PictureBox pictureBox2 = pictureBox;
+					puzzle.Add(pictureBox2);
+					puzzlePanel.Controls.Add(pictureBox2);
+				}
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			puzzlePanel.BackColor = BackColor;
+			btnCancel.Enabled = false;
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			numRow.SelectedIndex = -1;
+			numCol.SelectedIndex = -1;
+			btnCancel.Enabled = false;
+			btnShowHelp.Enabled = false;
+			//btnShowHelp.Checked = false;
+			btnGameSetting.Enabled = true;
+			numRow.Enabled = true;
+			numCol.Enabled = true;
+
+			for (int i = 0; i < puzzleSize; i++)
+			{
+				puzzlePanel.Controls.Remove(puzzle[i]);
+				puzzlePanel.Controls.Remove(cover[i]);
+			}
+			cover.Clear();
+			puzzle.Clear();
+			puzzleSize = 0;
+			TsProgress.Value = 0;
+			this.MinimumSize = new Size(420, 150);
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (MessageBox.Show("آیــا می خواهیـد خـارج شـوید", "Close", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+			{
+				e.Cancel = true;
+			}
+		}
+
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			HelpTimer.Stop();
+			//btnShowHelp.Checked = false;
+			for (int i = 0; i < puzzleSize; i++)
+			{
+				cover[i].Visible = true;
+			}
+		}
+
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+			ProgressTimer.Stop();
+			if (pushCover[0].Name != pushCover[1].Name)
+			{
+				pushCover[0].Visible = true;
+				pushCover[1].Visible = true;
+			}
+			else
+			{
+				TsProgress.PerformStep();
+				if (TsProgress.Value == TsProgress.Maximum)
+				{
+					MessageBox.Show("شما برنده شدید");
+				}
+			}
+			clickNumber = 0;
+			allowClick = true;
+		}
+
+		private void btnShowHelp_Click(object sender, EventArgs e)
+		{
+				for (int i = 0; i < puzzleSize; i++)
+				{
+					cover[i].Visible = false;
+				}
+				HelpTimer.Start();
+				btnShowHelp.Enabled = false;
+		}
+	}
+}
+
